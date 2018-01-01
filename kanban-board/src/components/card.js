5f04a0d74bc17961cdf98360735581bd47aa6671
Vitali Shcharbin <vitali.shcharbin@gmail.com>
2017-12-31 21:19:56 -0800

    Add toogle details to state.

--- kanban-board/src/components/card.js
+++ kanban-board/src/components/card.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import CheckList from './checklist'
 
 class Card extends Component {
+    constructor() {
+        // use spread operator to pass all arguments
+        super(...arguments);
+
+        // set default state (could be defined by props also)
+        this.state = {
+            showDetails: false
+        };
+    }
+
+    toggleDetails() {
+        this.setState({
+            showDetails: !this.state.showDetails
+        });
+    }
+
     render() {
-        return (
-            <div className="card">
-                <div className="card-title">{this.props.title}</div>
+        let cardDetails;
+
+        if (this.state.showDetails) {
+            cardDetails = (
                 <div className="card-details">
                     {this.props.description}
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
+            );
+        }
+        return (
+            <div className="card" onClick={this.toggleDetails.bind(this)}>
+                <div className="card-title">{this.props.title}</div>
+                {cardDetails}
             </div>
         );
     }
